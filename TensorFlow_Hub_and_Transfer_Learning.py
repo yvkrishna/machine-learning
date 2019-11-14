dede0c39aba5a5d6eaf8de2cedce24eb09a2d615
vedha krishna <49089434+yvkrishna@users.noreply.github.com>
2019-11-14 21:18:10 +0530

    Update TensorFlow_Hub_and_Transfer_Learning.py

--- TensorFlow_Hub_and_Transfer_Learning.py
+++ TensorFlow_Hub_and_Transfer_Learning.py
@@ -50,3 +50,32 @@ num_classes = info.features['label'].num_classes
 
 for i, example_image in enumerate(train_examples.take(3)):
   print("Image {} shape: {}".format(i+1, example_image[0].shape))
+
+def format_image(image, label):
+  image = tf.image.resize(image, (IMAGE_RES, IMAGE_RES))/255.0
+  return image, label
+
+BATCH_SIZE = 32
+
+train_batches      = train_examples.shuffle(num_examples//4).map(format_image).batch(BATCH_SIZE).prefetch(1)
+validation_batches = validation_examples.map(format_image).batch(BATCH_SIZE).prefetch(1)
+
+image_batch, label_batch = next(iter(train_batches.take(1)))
+image_batch = image_batch.numpy()
+label_batch = label_batch.numpy()
+
+result_batch = model.predict(image_batch)
+
+predicted_class_names = imagenet_labels[np.argmax(result_batch, axis=-1)]
+predicted_class_names
+
+plt.figure(figsize=(10,9))
+for n in range(30):
+  plt.subplot(6,5,n+1)
+  plt.subplots_adjust(hspace = 0.3)
+  plt.imshow(image_batch[n])
+  plt.title(predicted_class_names[n])
+  plt.axis('off')
+_ = plt.suptitle("ImageNet predictions")
+
+## complete part 3
