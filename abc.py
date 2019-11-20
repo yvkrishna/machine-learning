060f21c8b219c0993ebff2128e379d6618f3e34b
vedha krishna <49089434+yvkrishna@users.noreply.github.com>
2019-11-20 18:18:33 +0530

    Create abc.py

---
+++ abc.py
@@ -0,0 +1,76 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os
+import numpy as np
+import glob
+import shutil
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+_URL = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+
+zip_file = tf.keras.utils.get_file(origin=_URL,
+                                   fname="flower_photos.tgz",
+                                   extract=True)
+
+base_dir = os.path.join(os.path.dirname(zip_file), 'flower_photos')
+
+classes = ['roses', 'daisy', 'dandelion', 'sunflowers', 'tulips']
+
+for cl in classes:
+  img_path = os.path.join(base_dir, cl)
+  images = glob.glob(img_path + '/*.jpg')
+  print("{}: {} Images".format(cl, len(images)))
+  num_train = int(round(len(images)*0.8))
+  train, val = images[:num_train], images[num_train:]
+
+  for t in train:
+    if not os.path.exists(os.path.join(base_dir, 'train', cl)):
+      os.makedirs(os.path.join(base_dir, 'train', cl))
+    shutil.move(t, os.path.join(base_dir, 'train', cl))
+
+  for v in val:
+    if not os.path.exists(os.path.join(base_dir, 'val', cl)):
+      os.makedirs(os.path.join(base_dir, 'val', cl))
+    shutil.move(v, os.path.join(base_dir, 'val', cl))
+    
+round(len(images)*0.8)
+
+train_dir = os.path.join(base_dir, 'train')
+val_dir = os.path.join(base_dir, 'val')
+
+batch_size = 100
+IMG_SHAPE = 150 
+
+image_gen = ImageDataGenerator(rescale=1./255, horizontal_flip=True)
+
+train_data_gen = image_gen.flow_from_directory(
+                                                batch_size=batch_size,
+                                                directory=train_dir,
+                                                shuffle=True,
+                                                target_size=(IMG_SHAPE,IMG_SHAPE)
+                                                )
+                                                
+# This function will plot images in the form of a grid with 1 row and 5 columns where images are placed in each column.
+def plotImages(images_arr):
+    fig, axes = plt.subplots(1, 5, figsize=(20,20))
+    axes = axes.flatten()
+    for img, ax in zip( images_arr, axes):
+        ax.imshow(img)
+    plt.tight_layout()
+    plt.show()
+
+
+augmented_images = [train_data_gen[0][0][0] for i in range(5)]
+plotImages(augmented_images)
+
+image_gen = ImageDataGenerator(rescale=1./255, rotation_range=45)
+
+train_data_gen = image_gen.flow_from_directory(batch_size=batch_size,
+                                               directory=train_dir,
+                                               shuffle=True,
+                                               target_size=(IMG_SHAPE, IMG_SHAPE))
