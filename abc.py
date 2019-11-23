98a810e0ab9263abcbf47a05fbcfc32443fe7ca2
vedha krishna <49089434+yvkrishna@users.noreply.github.com>
2019-11-23 20:56:41 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -30,3 +30,23 @@ result.shape
 predicted_class = np.argmax(result[0], axis=-1)
 predicted_class
 
+labels_path = tf.keras.utils.get_file('ImageNetLabels.txt','https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt')
+imagenet_labels = np.array(open(labels_path).read().splitlines())
+
+plt.imshow(grace_hopper)
+plt.axis('off')
+predicted_class_name = imagenet_labels[predicted_class]
+_ = plt.title("Prediction: " + predicted_class_name.title())
+
+splits = tfds.Split.ALL.subsplit(weighted=(80, 20))
+
+splits, info = tfds.load('cats_vs_dogs', with_info=True, as_supervised=True, split = splits)
+
+(train_examples, validation_examples) = splits
+
+num_examples = info.splits['train'].num_examples
+num_classes = info.features['label'].num_classes
+
+for i, example_image in enumerate(train_examples.take(3)):
+  print("Image {} shape: {}".format(i+1, example_image[0].shape))
+
