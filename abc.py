3565d300f33f75f0ca6ea15be399d6dbb3d5febd
vedha krishna <49089434+yvkrishna@users.noreply.github.com>
2019-11-23 20:57:16 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -50,3 +50,21 @@ num_classes = info.features['label'].num_classes
 for i, example_image in enumerate(train_examples.take(3)):
   print("Image {} shape: {}".format(i+1, example_image[0].shape))
 
+def format_image(image, label):
+  image = tf.image.resize(image, (IMAGE_RES, IMAGE_RES))/255.0
+  return image, label
+
+BATCH_SIZE = 32
+
+train_batches      = train_examples.shuffle(num_examples//4).map(format_image).batch(BATCH_SIZE).prefetch(1)
+validation_batches = validation_examples.map(format_image).batch(BATCH_SIZE).prefetch(1)
+
+image_batch, label_batch = next(iter(train_batches.take(1)))
+image_batch = image_batch.numpy()
+label_batch = label_batch.numpy()
+
+result_batch = model.predict(image_batch)
+
+predicted_class_names = imagenet_labels[np.argmax(result_batch, axis=-1)]
+predicted_class_names
+
