d313f0f3fafe3251e694a03739c5bcebb241b36d
vedha krishna <49089434+yvkrishna@users.noreply.github.com>
2019-11-23 20:55:58 +0530

    Create abc.py

---
+++ abc.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+import matplotlib.pylab as plt
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+from tensorflow.keras import layers
+import logging
+logger = tf.get_logger()
+logger.setLevel(logging.ERROR)
+
+CLASSIFIER_URL ="https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/2"
+IMAGE_RES = 224
+
+model = tf.keras.Sequential([
+    hub.KerasLayer(CLASSIFIER_URL, input_shape=(IMAGE_RES, IMAGE_RES, 3))
+])
+
+import numpy as np
+import PIL.Image as Image
+
+grace_hopper = tf.keras.utils.get_file('image.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/grace_hopper.jpg')
+grace_hopper = Image.open(grace_hopper).resize((IMAGE_RES, IMAGE_RES))
+grace_hopper 
+
+grace_hopper = np.array(grace_hopper)/255.0
+grace_hopper.shape
+
+result = model.predict(grace_hopper[np.newaxis, ...])
+result.shape
+
+predicted_class = np.argmax(result[0], axis=-1)
+predicted_class
+
