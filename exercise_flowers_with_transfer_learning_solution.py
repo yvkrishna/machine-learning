abdc755a0dd9bdd138f05092ba99105dfead0abf
vedha krishna <49089434+yvkrishna@users.noreply.github.com>
2019-11-27 07:12:07 +0530

    Update exercise_flowers_with_transfer_learning_solution.py

--- exercise_flowers_with_transfer_learning_solution.py
+++ exercise_flowers_with_transfer_learning_solution.py
@@ -58,3 +58,97 @@ model = tf.keras.Sequential([
 ])
 
 model.summary()
+
+
+model.compile(
+  optimizer='adam',
+  loss='sparse_categorical_crossentropy',
+  metrics=['accuracy'])
+
+EPOCHS = 6
+
+history = model.fit(train_batches,
+                    epochs=EPOCHS,
+                    validation_data=validation_batches)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(EPOCHS)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+class_names = np.array(dataset_info.features['label'].names)
+
+print(class_names)
+
+image_batch, label_batch = next(iter(train_batches))
+
+
+image_batch = image_batch.numpy()
+label_batch = label_batch.numpy()
+
+predicted_batch = model.predict(image_batch)
+predicted_batch = tf.squeeze(predicted_batch).numpy()
+
+predicted_ids = np.argmax(predicted_batch, axis=-1)
+predicted_class_names = class_names[predicted_ids]
+
+print(predicted_class_names)
+
+print("Labels:           ", label_batch)
+print("Predicted labels: ", predicted_ids)
+
+plt.figure(figsize=(10,9))
+for n in range(30):
+  plt.subplot(6,5,n+1)
+  plt.subplots_adjust(hspace = 0.3)
+  plt.imshow(image_batch[n])
+  color = "blue" if predicted_ids[n] == label_batch[n] else "red"
+  plt.title(predicted_class_names[n].title(), color=color)
+  plt.axis('off')
+_ = plt.suptitle("Model predictions (blue: correct, red: incorrect)")
+
+IMAGE_RES = 299
+
+(training_set, validation_set), dataset_info = tfds.load('tf_flowers', with_info=True, as_supervised=True, split=splits)
+train_batches = training_set.shuffle(num_training_examples//4).map(format_image).batch(BATCH_SIZE).prefetch(1)
+validation_batches = validation_set.map(format_image).batch(BATCH_SIZE).prefetch(1)
+
+URL = "https://tfhub.dev/google/tf2-preview/inception_v3/feature_vector/4"
+feature_extractor = hub.KerasLayer(URL,
+  input_shape=(IMAGE_RES, IMAGE_RES, 3),
+  trainable=False)
+
+model_inception = tf.keras.Sequential([
+  feature_extractor,
+  tf.keras.layers.Dense(num_classes, activation='softmax')
+])
+
+model_inception.summary()
+
+model_inception.compile(
+  optimizer='adam', 
+  loss='sparse_categorical_crossentropy',
+  metrics=['accuracy'])
+
+EPOCHS = 6
+
+history = model_inception.fit(train_batches,
+                    epochs=EPOCHS,
+                    validation_data=validation_batches)
