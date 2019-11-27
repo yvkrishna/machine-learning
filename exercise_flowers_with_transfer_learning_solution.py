2de9fa137ee4833ce8d28917fe690a4891ffb2b4
vedha krishna <49089434+yvkrishna@users.noreply.github.com>
2019-11-27 07:10:51 +0530

    Create exercise_flowers_with_transfer_learning_solution.py

---
+++ exercise_flowers_with_transfer_learning_solution.py
@@ -0,0 +1,60 @@
+import tensorflow as tf
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+
+from tensorflow.keras import layers
+
+import logging
+logger = tf.get_logger()
+logger.setLevel(logging.ERROR)
+
+splits = tfds.Split.TRAIN.subsplit([70, 30])
+
+(training_set, validation_set), dataset_info = tfds.load('tf_flowers', with_info=True, as_supervised=True, split=splits)
+
+num_classes = dataset_info.features['label'].num_classes
+
+num_training_examples = 0
+num_validation_examples = 0
+
+for example in training_set:
+  num_training_examples += 1
+
+for example in validation_set:
+  num_validation_examples += 1
+
+print('Total Number of Classes: {}'.format(num_classes))
+print('Total Number of Training Images: {}'.format(num_training_examples))
+print('Total Number of Validation Images: {} \n'.format(num_validation_examples))
+
+for i, example in enumerate(training_set.take(5)):
+  print('Image {} shape: {} label: {}'.format(i+1, example[0].shape, example[1]))
+  
+IMAGE_RES = 224
+
+def format_image(image, label):
+  image = tf.image.resize(image, (IMAGE_RES, IMAGE_RES))/255.0
+  return image, label
+
+BATCH_SIZE = 32
+
+train_batches = training_set.shuffle(num_training_examples//4).map(format_image).batch(BATCH_SIZE).prefetch(1)
+
+validation_batches = validation_set.map(format_image).batch(BATCH_SIZE).prefetch(1)
+
+URL = "https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4"
+feature_extractor = hub.KerasLayer(URL,
+                                   input_shape=(IMAGE_RES, IMAGE_RES, 3))
+                                   
+feature_extractor.trainable = False
+
+model = tf.keras.Sequential([
+  feature_extractor,
+  layers.Dense(num_classes, activation='softmax')
+])
+
+model.summary()
