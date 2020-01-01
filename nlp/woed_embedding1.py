0f233ec591b6000286051eb3bfdf50418c0aea4d
vedha krishna <49089434+yvkrishna@users.noreply.github.com>
2020-01-01 21:02:47 +0530

    Update woed_embedding1.py

--- nlp/woed_embedding1.py
+++ nlp/woed_embedding1.py
@@ -109,3 +109,34 @@ print(weights.shape) # shape: (vocab_size, embedding_dim)
 
 
 
+import io
+
+out_v = io.open('vecs.tsv', 'w', encoding='utf-8')
+out_m = io.open('meta.tsv', 'w', encoding='utf-8')
+for word_num in range(1, vocab_size):
+  word = reverse_word_index[word_num]
+  embeddings = weights[word_num]
+  out_m.write(word + "\n")
+  out_v.write('\t'.join([str(x) for x in embeddings]) + "\n")
+out_v.close()
+out_m.close()
+
+
+
+
+
+
+try:
+  from google.colab import files
+except ImportError:
+  pass
+else:
+  files.download('vecs.tsv')
+  files.download('meta.tsv')
+  
+  
+  
+  
+sentence = "I really think this is amazing. honest."
+sequence = tokenizer.texts_to_sequences(sentence)
+print(sequence)
