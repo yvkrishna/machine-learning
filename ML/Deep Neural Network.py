9f9e5c5bb373e561405463c825ae8ba17fbfc002
vedha krishna <49089434+yvkrishna@users.noreply.github.com>
2020-01-18 22:08:10 +0530

    back propagation initialized and few bugs needed to be fixed

--- ML/Deep Neural Network.py
+++ ML/Deep Neural Network.py
@@ -245,3 +245,83 @@ print("Length of caches list = " + str(len(caches)))
 
 
 
+# GRADED FUNCTION: compute_cost
+
+def compute_cost(AL, Y):
+    """
+    Implement the cost function defined by equation (7).
+
+    Arguments:
+    AL -- probability vector corresponding to your label predictions, shape (1, number of examples)
+    Y -- true "label" vector (for example: containing 0 if non-cat, 1 if cat), shape (1, number of examples)
+
+    Returns:
+    cost -- cross-entropy cost
+    """
+    
+    m = Y.shape[1]
+
+    # Compute loss from aL and y.
+    ### START CODE HERE ### (≈ 1 lines of code)
+    cost = (-1/m) * np.sum(np.multiply(Y,np.log(AL))+np.multiply(1-Y,np.log(1-AL)))
+    ### END CODE HERE ###
+    
+    cost = np.squeeze(cost)      # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
+    assert(cost.shape == ())
+    
+    return cost
+
+
+
+
+Y, AL = compute_cost_test_case()
+
+print("cost = " + str(compute_cost(AL, Y)))
+
+
+
+
+# GRADED FUNCTION: linear_backward
+
+def linear_backward(dZ, cache):
+    """
+    Implement the linear portion of backward propagation for a single layer (layer l)
+
+    Arguments:
+    dZ -- Gradient of the cost with respect to the linear output (of current layer l)
+    cache -- tuple of values (A_prev, W, b) coming from the forward propagation in the current layer
+
+    Returns:
+    dA_prev -- Gradient of the cost with respect to the activation (of the previous layer l-1), same shape as A_prev
+    dW -- Gradient of the cost with respect to W (current layer l), same shape as W
+    db -- Gradient of the cost with respect to b (current layer l), same shape as b
+    """
+    A_prev, W, b = cache
+    m = A_prev.shape[1]
+
+    ### START CODE HERE ### (≈ 3 lines of code)
+    dW = np.dot(dZ, cache[0].T) / m
+    db = np.squeeze(np.sum(dZ, axis=1, keepdims=True)) / m
+    dA_prev = np.dot(cache[1].T, dZ)
+    ### END CODE HERE ###
+    
+    assert (dA_prev.shape == A_prev.shape)
+    assert (dW.shape == W.shape)
+    assert (isinstance(db, float))
+    
+    return dA_prev, dW, db
+
+
+
+
+# Set up some test inputs
+dZ, linear_cache = linear_backward_test_case()
+
+dA_prev, dW, db = linear_backward(dZ, linear_cache)
+print ("dA_prev = "+ str(dA_prev))
+print ("dW = " + str(dW))
+print ("db = " + str(db))
+
+
+
+
